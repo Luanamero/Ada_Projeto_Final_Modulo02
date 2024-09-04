8a9e5358347cf15450e1f50b8e472ae5c8e271b3
Victot <victorfarian@gmail.com>
2024-09-04 20:40:23 -0300

    Metodos uteis

--- projeto-final-POOI/src/edu/ada/agenda/util/MetodosUteis.java
+++ projeto-final-POOI/src/edu/ada/agenda/util/MetodosUteis.java
@@ -2,4 +2,15 @@ package edu.ada.agenda.util;
 
 public class MetodosUteis {
     //TODO fazer aqui os métodos para verificar os dados?
+    public boolean validaNome(String nome){
+        return false;
+    }
+
+    public boolean validaEmail(String email){
+        return false;
+    }
+
+    public boolean validaTelefone(String telefone){
+        return false;
+    }
 }
