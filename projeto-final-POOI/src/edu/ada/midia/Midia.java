fc53a7daaeebea0c21effb9fda3481f8ef1d6476
Victot <victorfarian@gmail.com>
2024-09-04 20:48:03 -0300

    abstrata de Midia

---
+++ projeto-final-POOI/src/edu/ada/midia/Midia.java
@@ -0,0 +1,35 @@
+package edu.ada.midia;
+
+public abstract class Midia {
+
+    protected String titulo;
+    protected String genero;
+    protected int anoLancamento;
+    protected int avaliacao;//de 1 a 5 estrelas
+
+    public Midia(String titulo, String genero, int anoLancamento) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+}
