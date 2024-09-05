b2a615f96b582c641fb31e9bbc6cd3f2e0bf17be
Catarine Goncalves <ccatarinegoncalves@gmail.com>
2024-09-04 22:00:46 -0300

    update: validation resources

--- projeto-final-POOI/src/edu/ada/util/MetodosUteis.java
+++ projeto-final-POOI/src/edu/ada/util/MetodosUteis.java
@@ -1,16 +1,44 @@
 package edu.ada.util;
 
 public class MetodosUteis {
-    //TODO fazer aqui os métodos para verificar os dados?
-    public boolean validaNome(String nome){
-        return false;
+    //TODO fazer aqui os métodos para verificar os dados
+
+    //    (verficar numero)
+    public boolean validaNome(String nome) {
+
+        if (nome == null || nome.isEmpty()) {
+            return false;
+        }
+
+
+        return true;
     }
 
-    public boolean validaEmail(String email){
-        return false;
+    public boolean validaEmail(String email) {
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+
+        int inicioIndex = email.indexOf('@');
+        int finalIndex = email.lastIndexOf('.');
+
+        if (inicioIndex <= 0 || finalIndex <= inicioIndex + 1 || finalIndex >= email.length() - 1) {
+            return false;
+        }
+
+        return finalIndex > inicioIndex + 1;
+
     }
 
-    public boolean validaTelefone(String telefone){
-        return false;
+    public boolean validaTelefone(String telefone) {
+        if (telefone == null || telefone.isEmpty()) {
+            return false;
+        }
+
+        if (telefone.length() != 10 && telefone.length() != 11) {
+            return false;
+        }
+
+        return true;
     }
 }
