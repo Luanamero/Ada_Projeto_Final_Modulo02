8c8e2d63f9c8296b40beee37652e65ef63a9d620
Victot <victorfarian@gmail.com>
2024-09-08 09:29:54 -0300

    merge

--- projeto-final-POOI/src/edu/ada/view/Aba.java
+++ projeto-final-POOI/src/edu/ada/view/Aba.java
@@ -1,15 +1,12 @@
 package edu.ada.view;
 
 import edu.ada.agenda.Agenda;
-
-
 import edu.ada.agenda.Contato;
 import edu.ada.midia.Midia;
 
 import java.util.Scanner;
 
 public class Aba {
-    ;
     Scanner sc = new Scanner(System.in);
 
     public void cadastrar(Agenda agenda){
@@ -32,56 +29,47 @@ public class Aba {
         String resposta = sc.nextLine().toUpperCase();
 
         if(resposta.equals("S")){
-            SubAbas subAbas = new SubAbas();
-            Midia midia = subAbas.abrirOpcao();
-            contato.addMidia(midia);
+        SubAbas subAbas = new SubAbas();
+        Midia midia = subAbas.abrirOpcao();
+        contato.addMidia(midia);
 
         }
         agenda.addContato(contato);
 
-    };
+            };
     public void listarTodos(Agenda agenda){
         agenda.getContatos();
     };
 
     public void listar(Agenda agenda){
-        if (agenda.verificaVazio())return;
+        if (agenda.verificaVazio()){}else {
         System.out.println("Digite o id do contato");
         int id = sc.nextInt();
 
-        Contato contato = agenda.getContato(id);
-        if (contato != null) {
-            System.out.println(contato);
-        } else {
-            System.out.println("Contato com ID " + id + " não encontrado.");
+            Contato contato = agenda.getContato(id);
+            if (contato != null) {
+                               System.out.println(contato);
+            } else {
+                System.out.println("Contato com ID " + id + " não encontrado.");
+            }
         }
-
     }
 
     public void editar(Agenda agenda){
-        if (agenda.verificaVazio())return;
+        if (agenda.verificaVazio()){}else {
 
-        System.out.println("Digite o id do contato");
-        int id = Integer.parseInt(sc.nextLine());
-        System.out.println("Digite o nome do contato");
-        String nome = sc.nextLine();
-        System.out.println("Digite o tele do contato");
-        String telefone = sc.nextLine();
-        System.out.println("Digite o email do contato");
-        String email = sc.nextLine();
-
-        agenda.editarContato(id, nome, telefone, email);
-        if (agenda.editarContato(id, nome, telefone, email)!=null){
-            System.out.println("Contato atualizado com sucesso.");
-        }else{ System.out.println("Contato com id: " +id + " não encontrado.");}
-
-    }
+            System.out.println("Digite o id do contato");
+            System.out.println("Digite o nome do contato");
+            System.out.println("Digite o tele do contato");
+            System.out.println("Digite o email do contato");
+            agenda.editarContato(Integer.parseInt(sc.nextLine()), sc.nextLine(), sc.nextLine(), sc.nextLine());
+        }
+    };
 
     public void excluir(Agenda agenda){
-        if (agenda.verificaVazio())return;
-
-            System.out.println("Digite o Id do contato a ser removido");
-            int id = sc.nextInt();
-            agenda.removeContato(id);
-        }
+        if (agenda.verificaVazio()){}else {
+        System.out.println("Digite o Id do contato a ser removido");
+        int id = sc.nextInt();
+        agenda.removeContato(id);
+    }}
 }
--- projeto-final-POOI/src/edu/ada/view/Menu.java
+++ projeto-final-POOI/src/edu/ada/view/Menu.java
@@ -18,25 +18,25 @@ public class Menu {
         System.out.println("4. Editar Contato");
         System.out.println("5. Excluir Contato");
         System.out.println("0. Sair");
-        System.out.println();
+        System.out.println("");
 
         int opcao =  Integer.parseInt(sc.nextLine());
         boolean sair = false;
 
         switch (opcao) {
-            case 1 -> aba.cadastrar(agenda);
-            case 2 -> aba.listarTodos(agenda);
-            case 3 -> aba.listar(agenda);
-            case 4 -> aba.editar(agenda);
-            case 5 -> aba.excluir(agenda);
+            case 1 -> {aba.cadastrar(agenda);}
+            case 2 -> {aba.listarTodos(agenda);}
+            case 3 -> {aba.listar(agenda);}
+            case 4 -> {aba.editar(agenda);}
+            case 5 -> {aba.excluir(agenda); }
             case 0 -> {System.out.println("Saindo...");
                 sair = true;
             }
             default -> {System.out.println("Insira uma opção válida.");
                 abrirMenu();}
         }
-        if (!sair){
-            abrirMenu();}
+        if (sair==false){
+        abrirMenu();}
     }
 
 }
